8f2762c05b4a408fdcb6f2482a5eae604809cb7c
Mazum Paudel <mazumpaudel02@gmail.com>
2026-03-31 13:28:59 +0545

    Initial commit

--- tests/pinevisa.spec.js
+++ tests/pinevisa.spec.js
@@ -23,6 +23,44 @@ test.describe('Pinevisa Automation', () => {
         await expect(experienceOption).toBeVisible();
         await experienceOption.click();
 
+        const continueButton = page.getByRole('button', { name: 'Continue' });
+        await expect(continueButton).toBeVisible();
+        await continueButton.click();
+
+        //Preferred Visa Program
+
+        // Open the "Select program" dropdown
+        const programDropdown = page.getByRole('button', { name: 'Select program' });
+        await expect(programDropdown).toBeVisible();
+        await programDropdown.click();
+
+        // Click on the "J-1 Visa" option
+        const j1VisaOption = page.getByRole('button', { name: 'J-1 Visa' });
+        await expect(j1VisaOption).toBeVisible();
+        await j1VisaOption.click();
+
+        const continueBtn = page.getByRole('button', { name: 'Continue' });
+        await expect(continueBtn).toBeVisible();
+        await continueBtn.click();
+
+        const fullNameInput = page.locator('input[name="full_name"]');
+        await fullNameInput.fill('Mazum Paudel');
+
+        const emailInput = page.locator('input[name="email"]');
+        await emailInput.fill('mazumpaudel02@gmail.com');
+
+        const phoneInput = page.locator('input[name="phone_number"]');
+        await phoneInput.fill('+977-9813928426');
+
+        const SubmitButton = page.getByRole('button', { name: 'Submit' });
+        await expect(SubmitButton).toBeVisible();
+        await SubmitButton.click();
+
+
+
+
+
+     
 
         // Verify selection
     });
