36901933bdadf2cdd161f15bcf4bd520746c6817
Mazum Paudel <mazumpaudel02@gmail.com>
2026-04-09 17:29:09 +0545

    Initial commit

--- tests/pinevisa.createnewadmin.spec.js
+++ tests/pinevisa.createnewadmin.spec.js
@@ -5,13 +5,19 @@ test.describe('Pinevisa Automation', () => {
         await page.goto('https://staging.pinevisa.com/admin');
 
         await page.locator('input[id="form.email"]').fill('admin@pinevisa.com');
-        await page.locator('input[id="form.password"]').fill('pinevisa@2026');
-        
+        await page.locator('input[id="form.password"]').fill('pinevisa#2026');
+
         // Click Remember Me checkbox
         await page.locator('input[id="form.remember"]').click();
 
         // Click Sign In button
         await page.getByRole('button', { name: 'Sign in' }).click();
+
+        // Step 1: Click the avatar to open the user dropdown
+        await page.getByRole('button', { name: 'Avatar of Admin Kumar' }).click();
+
+        // Step 2: Click "Enable dark theme" from the dropdown
+        await page.getByRole('button', { name: 'Enable dark theme' }).click();
     });
 });
 
--- tests/pinevisa.readourstory.spec.js
+++ tests/pinevisa.readourstory.spec.js
@@ -3,17 +3,9 @@ const { test, expect } = require('@playwright/test');
 test.describe('Pinevisa Automation', () => {
     test('Fill nationality and select experience', async ({ page }) => {
         await page.goto('https://staging.pinevisa.com/');
+        
 
-        await page.getByRole('link', { name: 'Read Our Story' }).click();
-        await page.getByRole('link', { name: 'Start Free Consultation' }).click();
 
-        await page.locator('input[name="full_name"]').fill('Mazum Paudel');
-        await page.locator('input[name="email"]').fill('mazumpaudel02@gmail.com');
-        await page.locator('input[name="phone"]').fill('9813928426');
-        await page.locator('input[name="industry"]').fill('IT');
-        await page.locator('textarea[name="message"]').fill('I am interested in your services.');
-        await page.getByRole('button', { name: 'Submit' }).first().click(); 
-        await page.waitForTimeout(1000);
 
 
 
